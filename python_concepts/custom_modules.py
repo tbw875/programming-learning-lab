531162b1b41441c2ee164722571a0766095fe01c
tbw875 <charger875@gmail.com>
2025-01-20 22:28:05 -0800

    Custom Modules

---
+++ python_concepts/custom_modules.py
@@ -0,0 +1,10 @@
+# CUSTOM MODULES
+
+# We can import custom modules by importing the file name directly in the script itself.
+
+import example_module
+
+example_module.greeting("Tom")
+
+# Because the example module has the function "Hello + name" 
+# this prints "Hello Tom"
---
+++ python_concepts/example_module.py
@@ -0,0 +1,6 @@
+# EXAMPLE MODULE
+
+# To be imported into custom_modules.py
+
+def greeting(name):
+    print("Hello " + name)
